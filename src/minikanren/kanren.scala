fe5a421bbc2927a81eabd3a773c3aa137fc78e2c
Alex Gryzlov <alex.gryzlov@gmail.com>
2013-11-24 01:57:15 +0300

    appendo finally works
    lists are still somewhat cumbersome

--- src/minikanren/kanren.scala
+++ src/minikanren/kanren.scala
@@ -17,7 +17,7 @@ object kanren {
 
   // 2
 
-  sealed class Term
+  sealed abstract class Term
   case class Var(name: String) extends Term
   case class IntVal(i: Int) extends Term
   case class ListVal(l: List[Term]) extends Term
@@ -40,41 +40,40 @@ object kanren {
         }
     }
 
-  def unify(t1: Term, t2: Term): Subst => Option[Subst] = { s: Subst =>
-    val (tt1, tt2) = (lookup(t1, s), lookup(t2, s))
-    //   println("LOOKUP " + tt1 + " " + tt2)
-    (tt1, tt2) match {
-      case (a, b) if (a == b) => Some(s)
-      case (Var(x), y) => Some(extend(Var(x), y, s))
-      case (x, Var(y)) => Some(extend(Var(y), x, s))
-      case (ListVal(l1), ListVal(l2)) => (l1, l2) match {
-        case (l1v :: Nil, _) => unify(l1v, ListVal(l2))(s)
-        case (_, l2v :: Nil) => unify(ListVal(l1), l2v)(s)
-        case (l1h :: l1t, l2h :: l2t) => unify(l1h, l2h)(s).flatMap {
-          unify(ListVal(l1t), ListVal(l2t))
+  def lookup2(v: Term, s: Subst): Term = lookup(v, s) match {
+    case Var(x) => Var(x)
+    case IntVal(x) => IntVal(x)
+    case ListVal(x) if (!x.isEmpty) =>
+      if (x.tail.isEmpty) lookup2(x.head, s) else ListVal(List(lookup2(x.head, s), lookup2(ListVal(x.tail), s)))
+    case ListVal(List()) => ListVal(List())
+  }
+
+  def unify(t1: Term, t2: Term): Subst => Nond[Subst] = { s: Subst =>
+    (lookup(t1, s), lookup(t2, s)) match {
+      case (a, b) if (a == b) => succeed(s)
+      case (Var(x), y) => succeed(extend(Var(x), y, s))
+      case (x, Var(y)) => succeed(extend(Var(y), x, s))
+      case (ListVal(l1), ListVal(l2)) if (!l1.isEmpty && !l2.isEmpty) =>
+        (l1, l2) match {
+          case (l1h :: l1t, l2h :: l2t) if (l1t.size == 1) => unify(l1h, l2h)(s).flatMap(unify(l1t.head, ListVal(l2t)))
+          case (l1h :: l1t, l2h :: l2t) if (l2t.size == 1) => unify(l1h, l2h)(s).flatMap(unify(ListVal(l1t), l2t.head))
+          case (l1h :: l1t, l2h :: l2t) => unify(l1h, l2h)(s).flatMap(unify(ListVal(l1t), ListVal(l2t)))
         }
-      }
-      //case (Right(xx), Right(yy)) => unify(xx, yy, s)
-      case _ => None
+      case _ => fail(s)
     }
 
   }
 
   // 3
 
-  def equate(t1: Term, t2: Term) = { s: Subst =>
-    unify(t1, t2)(s) match {
-      case Some(s) => succeed(s)
-      case None => fail(s)
-    }
-  }
-
   def run(g: Subst => Nond[Subst]) = g(emptySubst)
+
+  def run2(g: Subst => Nond[Subst]) = g(emptySubst).map { x => lookup2(Var("q"), x) }
 
   def choice(v: Term, lst: List[Term]): Subst => Nond[Subst] = lst match {
     case Nil => fail[Subst]
     case hd :: tl =>
-      disj(equate(v, hd), choice(v, tl))
+      disj(unify(v, hd), choice(v, tl))
   }
 
   def commonElement(l1: List[Term], l2: List[Term]) = {
@@ -85,34 +84,37 @@ object kanren {
   }
 
   def conso(a: Term, b: Term, l: Term) = b match {
-    case ListVal(lb) => equate(ListVal(a +: lb), l)
-    case _ => equate(ListVal(List(a, b)), l)
+    case ListVal(lb) => unify(ListVal(a +: lb), l)
+    case _ => unify(ListVal(List(a, b)), l)
   }
 
   def appendo(l1: Term, l2: Term, l3: Term): Subst => Nond[Subst] = {
-    disj(
-      conj(equate(l1, ListVal(Nil)), equate(l2, l3)),
-      {
-        val h = Var("h")
-        val t = Var("t")
-        val l3p = Var("l3p")
-        conj(
-          conso(h, t, l1),
-          { s: Subst =>
-            conj(
-              conso(h, l3p, l3),
-              { s: Subst =>
-                appendo(t, l2, l3p)(s)
-              })(s)
-          })
-      })
+    def appendoHelper(l1: Term, l2: Term, l3: Term)(depth: Int): Subst => Nond[Subst] = {
+      disj(
+        conj(unify(l1, ListVal(Nil)), unify(l2, l3)),
+        {
+          val h = Var("h" + depth)
+          val t = Var("t" + depth)
+          val l3p = Var("l3p" + depth)
+          conj(
+            conso(h, t, l1),
+            { s1: Subst =>
+              conj(
+                conso(h, l3p, l3),
+                { s2: Subst =>
+                  appendoHelper(t, l2, l3p)(depth + 1)(s2)
+                })(s1)
+            })
+        })
+    }
+    appendoHelper(l1, l2, l3)(0)
   }
 
   // TESTS
 
-  def cout(name: String)(subs: List[Subst]) = {
+  def cout[A](name: String)(as: List[A]) = {
     println(name)
-    println(subs)
+    println(as)
   }
 
   def test1 {
@@ -128,13 +130,13 @@ object kanren {
     val vx = Var("x")
     val vy = Var("y")
 
-    val xy = unify(vx, vy)(emptySubst).get
-    cout("test-u1")(xy :: Nil)
+    val xy = unify(vx, vy)(emptySubst)
+    cout("test-u1")(xy)
     println("test-u2")
-    val xy1 = unify(vx, IntVal(1))(xy).get
+    val xy1 = unify(vx, IntVal(1))(xy.head)
     println(xy1)
     println("test-u3")
-    println(lookup(vx, xy1))
+    println(lookup(vx, xy1.head))
   }
 
   def test3 {
@@ -167,9 +169,21 @@ object kanren {
   }
 
   def testAp {
+    val vx = Var("x")
+    val vy = Var("y")
     val vq = Var("q")
-    cout("t1")(run(appendo(wrapL(1), wrapL(2), vq)))
 
+    cout("t0")(run(appendo(wrapL(1), wrapL(2), vq)))
+    cout("t1")(run2(appendo(wrapL(1), wrapL(2), vq)))
+    cout("t2")(run2(appendo(wrapL(1), wrapL(2), wrapL(1))))
+    cout("t3")(run2(appendo(wrapL(1, 2, 3), vq, wrapL(1, 2, 3, 4, 5))))
+    cout("t4")(run2(appendo(vq, wrapL(4, 5), wrapL(1, 2, 3, 4, 5))))
+    cout("t5")(run2(appendo(vq, vx, wrapL(1, 2, 3, 4, 5))))
+    cout("t6")(run2(appendo(vx, vq, wrapL(1, 2, 3, 4, 5))))
+    cout("t7")(run2(
+      conj(
+        appendo(vx, vy, wrapL(1, 2, 3, 4, 5)),
+        unify(vq, ListVal(List(vx, vy))))))
   }
 
   def main(args: Array[String]) {
